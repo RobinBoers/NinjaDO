99e466325cffd95955f4a055e0bea798ed6f8b3c
Robin Boers <60298132+RobinBoers@users.noreply.github.com>
2020-12-24 20:33:33 +0100

    Added comments
    To make the code more readable. Also added a little hack for anyone that is reading the code...

--- game.js
+++ game.js
@@ -1,5 +1,6 @@
 // CONSTANTS
 
+// Image sizes
 const canvasWidth = 800;
 const canvasHeight = 600;
 const bgWidth = 1000;
@@ -9,12 +10,22 @@ const playerHeight = 70;
 const playerImageW = 6;
 const playerImageH = 7;
 
+// Keycodes
 const upKeyCode = 87;
 const downKeyCode = 83;
 const rightKeyCode = 68;
 const leftKeyCode = 65;
-const playerSpeed = 8;
+const hackKeyCode = 59;
 
+// Movement speed
+var playerSpeed = 8;
+const superSpeed = 30;
+
+// Abilities
+const jumpKeyCode = 87;
+const jumpPower = 8;
+
+// Animated sprites
 const spriteFramesPerRow = 2;
 const totalFrames = 2;
 const AnimationSpeed = 10;
@@ -28,17 +39,22 @@ canvas.height = canvasHeight;
 document.body.appendChild(canvas);
 var i = 0;
 
+// Animated sprite used when
+// the player is moving
 var playerImage = new Image();
 playerImage.src = 'assets/playerSprites2.png';
 
+// Image used if the player isn't moving
 var stillPlayerImage = new Image();
 stillPlayerImage.src = 'assets/player.png';
 
 spriteFrameNum = 0;
 
+// Image for scene
 var background = new Image();
 background.src = 'assets/background.png';
 
+// Player movement and location
 var playerX = 50;
 var playerY = 40;
 var playerYspeed = 0;
@@ -49,11 +65,16 @@ var runningD = false;
 var lookingL = false;
 var lookingR = true;
 var moving = false;
+var hacking = false;
 
 var camX = 0;
 var camY = 0;
 
+// Framcounter, used for the animated sprite
 var frameCounter = 0;
+
+// Overwrite which will stop the
+// game is set to true
 var stop = false;
 
 // Event listeners
@@ -83,6 +104,7 @@ function onKeydown(event) {
     if(event.keyCode === downKeyCode) runningD = true;
     if(event.keyCode === rightKeyCode) runningR = true;
     if(event.keyCode === leftKeyCode) runningL = true;
+    if(event.keyCode === hackKeyCode) hacking = true;
 }
 
 function onKeyup(event) {
@@ -103,6 +125,12 @@ function update() {
     frameCounter = frameCounter + 1;
     document.querySelector("#frameNum").innerHTML = i;
 
+    // Superspeed for hackers
+    // Is triggerd by the ; key
+    if(hacking) {
+        playerSpeed = superSpeed;
+    }
+
     // Update player movement
     if(runningU) {
         playerY = playerY - playerSpeed;
@@ -121,6 +149,7 @@ function update() {
         lookingL = false;
     }
 
+    // Check if moving, used later to determin image to use
     if(runningR || runningD || runningL || runningU) moving = true;
     else moving = false;
 
@@ -129,7 +158,6 @@ function update() {
     camY = playerY - 300;
 
     // Update player animation
-
     if((frameCounter % AnimationSpeed) === 0) {
         spriteFrameNum = spriteFrameNum + 1;
         if(spriteFrameNum >= totalFrames) {
@@ -141,6 +169,8 @@ function update() {
     document.querySelector("#corX").innerHTML = playerX;
     document.querySelector("#corY").innerHTML = playerY;
 
+    // Show direction the player is 
+    // lookin at in HUD.
     if(lookingL) {
         document.querySelector("#dir").innerHTML = "L";
     } else {
@@ -152,8 +182,11 @@ function update() {
 
 function draw() {
 
+    // Save the non-flipped state to restore later
     c.save();
 
+    // Make sure the pixelart
+    // isn't ruined
     c.imageSmoothingEnabled = false;
     c.webkitImageSmoothingEnabled = false;
 
@@ -176,36 +209,50 @@ function draw() {
     var spriteX = spritesCol * playerImageW;
     var spriteY = spritesRow * playerImageH;
 
+    // If the player is moving,
+    // use a animated sprite (from spritesheet)
     if(moving) {
-        // check for player direction, and flip the image the right way
+
+        // Check for player direction, and flip the image the right way
         if(lookingL) {
 
             // Flip canvas
             c.translate(canvasWidth, 0);
             c.scale(-1, 1);
             
-            // Draw the player
+            // Put the player onscreen
             c.drawImage(playerImage, spriteX, spriteY, playerImageW, playerImageH, playerX + 150 - camX, playerY - camY, playerWidth, playerHeight);
 
-            // Remove flip
+            // Remove flip by restoring
+            // the state saved at the start
+            // of the draw function
             c.restore();
 
         } else {
+            // Put the player onscreen
             c.drawImage(playerImage, spriteX, spriteY, playerImageW, playerImageH, playerX - camX, playerY - camY, playerWidth, playerHeight);
         }
+
+    // If the player isn't moving, just use a still sprite
     } else {
+
+        // Check for player direction, and flip the image the right way
         if(lookingL) {
             
             // Flip canvas
             c.translate(canvasWidth, 0);
             c.scale(-1, 1);
 
+            // Put the player onscreen
             c.drawImage(stillPlayerImage, playerX + 150 - camX, playerY - camY, playerWidth, playerHeight);
 
-            // Remove flip
+            // Remove flip by restoring
+            // the state saved at the start
+            // of the draw function
             c.restore();
 
         } else {
+            // Put the player onscreen
             c.drawImage(stillPlayerImage, playerX - camX, playerY - camY, playerWidth, playerHeight);
         }
         
