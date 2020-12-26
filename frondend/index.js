d04540ffaabfac0d7cc3d987b1b6710c6764c6c2
Robin Boers <60298132+RobinBoers@users.noreply.github.com>
2020-12-26 18:18:20 +0100

    Made dead players invisible

--- frondend/index.js
+++ frondend/index.js
@@ -228,6 +228,8 @@ function draw(state, playerNum) {
 
 function drawPlayer(player, camX, camY) {
 
+    if(player.dead) return;
+
     var playerX = player.pos.x;
     var playerY = player.pos.y;
     var moving = player.moving;
